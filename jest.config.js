71efc3cade4c92d128b29331b23b31a57bd53413
ArazEstPrit <oruclu.araz@gmail.com>
2024-05-04 00:41:26 +0400

    feat(tests): Add testing framework; initial test cases on createTask
    Added testing framework Jest to the project. Made a few test cases for createTask function. Added test to npm scripts

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('jest').Config} */
+const config = {
+	transform: {},
+};
+
+export default config;
---
+++ tests/taskService.test.js
@@ -0,0 +1,55 @@
+import { jest } from "@jest/globals";
+import { writeFile } from "../services/fileService.js";
+import { createTask, getTasks } from "../services/taskService.js";
+
+describe("createTask", () => {
+	beforeEach(() => {
+		writeFile("./database/tasks.json", JSON.stringify([], null, "\t"));
+	});
+
+	beforeEach(() => {
+		jest.spyOn(console, "log");
+		jest.spyOn(console, "error");
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
+	});
+
+	it("should create a new task", () => {
+		createTask("Test Task", "2026-01-01");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(1);
+		expect(tasks[0].name).toBe("Test Task");
+		expect(tasks[0].dueDate).toBe(new Date("2026-01-01").toISOString());
+		expect(console.log).toHaveBeenCalledWith("Task \"Test Task\" created!");
+	});
+
+	it("should not allow a task with an invalid due date", () => {
+		createTask("Invalid Due Date", "202502-31");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(0);
+		expect(console.error).toHaveBeenCalledWith("Invalid due date");
+	});
+
+	it("should not allow a task with a due date in the past", () => {
+		createTask("Past Due Date", "2020-01-01");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(0);
+		expect(console.error).toHaveBeenCalledWith("Due date must be in the future");
+	});
+
+	it("should not allow a duplicate task", () => {
+		createTask("Duplicate Task", "2026-01-01");
+		createTask("Duplicate Task", "2026-01-01");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(1);
+		expect(console.error).toHaveBeenCalledWith("A task with the same name and due date already exists");
+	});
+});
+
+
+
+
+
+
