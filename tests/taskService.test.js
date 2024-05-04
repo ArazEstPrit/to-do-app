38b0582d5079d7eeb93f405fb8ac28f49e497dbd
ArazEstPrit <oruclu.araz@gmail.com>
2024-05-04 16:30:15 +0400

    test(tests): Add deleteTask tests
    Made tests for deleteTask

--- tests/taskService.test.js
+++ tests/taskService.test.js
@@ -1,13 +1,11 @@
 import { jest } from "@jest/globals";
 import { writeFile } from "../services/fileService.js";
-import { createTask, getTasks } from "../services/taskService.js";
+import { createTask, getTasks, deleteTask } from "../services/taskService.js";
 
 describe("createTask", () => {
 	beforeEach(() => {
 		writeFile("./database/tasks.json", JSON.stringify([], null, "\t"));
-	});
 
-	beforeEach(() => {
 		jest.spyOn(console, "log");
 		jest.spyOn(console, "error");
 	});
@@ -49,6 +47,68 @@ describe("createTask", () => {
 		expect(tasks).toHaveLength(1);
 		expect(console.error).toHaveBeenCalledWith(
 			"A task with the same name and due date already exists"
+		);
+	});
+});
+
+describe("deleteTask", () => {
+	beforeEach(() => {
+		writeFile(
+			"./database/tasks.json",
+			JSON.stringify(
+				[
+					{
+						name: "Test Task 1",
+						dueDate: new Date("2026-01-01").toISOString(),
+					},
+					{
+						name: "Test Task 2",
+						dueDate: new Date("2026-01-02").toISOString(),
+					},
+					{
+						name: "Duplicate Task",
+						dueDate: new Date("2026-01-03").toISOString(),
+					},
+					{
+						name: "Duplicate Task",
+						dueDate: new Date("2026-01-04").toISOString(),
+					},
+				],
+				null,
+				"\t"
+			)
+		);
+
+		jest.spyOn(console, "log");
+		jest.spyOn(console, "error");
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
+		writeFile("./database/tasks.json", JSON.stringify([], null, "\t"));
+	});
+
+	it("should delete a task", () => {
+		deleteTask("Test Task 1");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(3);
+		expect(tasks[0].name).toBe("Test Task 2");
+		expect(console.log).toHaveBeenCalledWith("Task \"Test Task 1\" deleted!");
+	});
+
+	it("should not delete a task that does not exist", () => {
+		deleteTask("Non-Existent Task");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(4);
+		expect(console.error).toHaveBeenCalledWith("Task not found");
+	});
+
+	it("should delete multiple tasks with the same name and due date", () => {
+		deleteTask("Duplicate Task");
+		let tasks = getTasks();
+		expect(tasks).toHaveLength(3);
+		expect(console.log).toHaveBeenCalledWith(
+			"Task \"Duplicate Task\" deleted!"
 		);
 	});
 });
