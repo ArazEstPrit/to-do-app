fc1653e660499983aee92a02c03da8d508e947f6
ArazEstPrit <oruclu.araz@gmail.com>
2024-07-27 13:26:04 +0400

    feat(gui): add button ui element

--- src/views/web/App.tsx
+++ src/views/web/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./styles/main.scss";
+import { Button } from "./components/Button.tsx";
 
 export function App() {
-	return <>
-		<h1>To-Do App</h1>
-	</>;
+	return (
+		<>
+			<h1>To-Do App</h1>
+			<Button
+				text="Hello World!"
+				color="main"
+				weight="primary"
+				onClick={() => {}}
+			/>
+		</>
+	);
 }
